6edd77dd229d2c122d849fc64288891bb01a4e94
Thúy Diễm <86779411+Baebaechuu@users.noreply.github.com>
2021-07-01 18:34:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Xem và trả lời hết nhó',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Iu em nhìu nhìu nhìu <33',
     btnNo: 'Không nha con tró :))',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại chọn iu em dọ <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho em iu <3',
+    reply: 'Vì em quá ngọt, em rất khác bọt :3 ',
     mess: 'Em biết màaa 🥰. Mãi bên nhao bạn nhó 😘😘',
-    messDesc: 'Ê thèm ts quá ik :<.',
+    messDesc: 'Ê thèm ts quá, mua cho em ik :<.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
